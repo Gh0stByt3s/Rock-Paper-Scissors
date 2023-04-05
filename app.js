63364373cd55809c2a94a9ce4a904d86391d6134
Gh0stByt3s <orangageorge.gr@gmail.com>
2023-04-05 16:57:48 +0300

    Finished game UI

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const title = document.querySelector(".title");
+const title = document.querySelector(".modal_title");
 const buttons = document.querySelectorAll("button");
 const player_score = document.querySelector(".player_score");
 const computer_score = document.querySelector(".computer_score");
+const modal = document.querySelector(".modal");
+const resetBtn = document.querySelector(".modal_button");
+const overlay = document.querySelector(".overlay");
+const comp_choice = document.querySelector(".comp_selection");
+const ties = document.querySelector(".ties");
 let pscore = 0;
 let cscore = 0;
+let tie = 0;
 
 function getComputerChoice() {
   num = Math.floor(Math.random() * 3) + 1;
 
   if (num === 1) {
+    comp_choice.src = "assets/fist.png";
     return "Rock";
   } else if (num === 2) {
+    comp_choice.src = "assets/palm-of-hand.png";
     return "Paper";
   } else {
+    comp_choice.src = "assets/scissor.png";
     return "Scissors";
   }
 }
@@ -26,7 +35,10 @@ buttons.forEach((button) =>
 function game(playerOption) {
   let computerSelection = getComputerChoice();
 
-  if (playerOption === computerSelection) return;
+  if (playerOption === computerSelection) {
+    tie++;
+    return (ties.textContent = `Ties: ${tie}`);
+  }
   if (playerOption === "Rock") {
     if (computerSelection === "Paper") {
       cscore++;
@@ -62,7 +74,8 @@ function game(playerOption) {
 
 function winner() {
   if (cscore === 5 || pscore === 5) {
-    buttons.forEach((button) => button.setAttribute("disabled", true));
+    modal.classList.add("modal_active");
+    overlay.classList.add("active");
   }
   if (cscore === 5) {
     title.textContent = "Game over!! Computer Wins!! ";
@@ -72,16 +85,18 @@ function winner() {
   }
 }
 
-const resetBtn = document.createElement("button");
-resetBtn.innerText = "Restart Game";
-document.body.appendChild(resetBtn);
 resetBtn.addEventListener("click", startGame);
+overlay.addEventListener("click", startGame);
 
 function startGame() {
   pscore = 0;
   cscore = 0;
+  tie = 0;
   computer_score.textContent = cscore;
   player_score.textContent = pscore;
-  buttons.forEach((button) => button.removeAttribute("disabled"));
-  title.textContent = "Start Game";
+  ties.textContent = `Ties: ${tie}`;
+  title.textContent = "";
+  comp_choice.src = "";
+  modal.classList.remove("modal_active");
+  overlay.classList.remove("active");
 }
